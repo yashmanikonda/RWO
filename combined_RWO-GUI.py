ed73d293933f8afeb741e1f47576305641ba0b8b
Yash Manikonda <90308705+yashmanikonda@users.noreply.github.com>
2023-08-12 00:29:40 +0530

    Update combined_RWO-GUI.py

--- combined_RWO-GUI.py
+++ combined_RWO-GUI.py
@@ -9,6 +9,18 @@ from selenium.webdriver.support import expected_conditions as EC
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome()
 
+def get_repository_links():
+    # Wait for the repositories list to load
+    repositories_list_div = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'user-repositories-list'))
+    )
+
+    # Find all anchor elements (links) within the div
+    links = repositories_list_div.find_elements(By.TAG_NAME, 'a')
+
+    # Extract and return the repository URLs
+    return [link.get_attribute('href') for link in links]
+
 class LoginApp(QWidget):
     def __init__(self):
         super().__init__()
@@ -26,6 +38,7 @@ class LoginApp(QWidget):
         self.password_input.setEchoMode(QLineEdit.Password)
 
         self.login_button = QPushButton("Login")
+        self.close_button = QPushButton("Exit")
 
         layout = QVBoxLayout()
         layout.addWidget(self.username_label)
@@ -33,27 +46,62 @@ class LoginApp(QWidget):
         layout.addWidget(self.password_label)
         layout.addWidget(self.password_input)
         layout.addWidget(self.login_button)
+        layout.addWidget(self.close_button)
 
         self.setLayout(layout)
         self.login_button.clicked.connect(self.on_login)
+        self.close_button.clicked.connect(self.close_app)
 
     def on_login(self):
-        username = self.username_input.text()
-        password = self.password_input.text()
+            username = self.username_input.text()
+            password = self.password_input.text()
 
-        driver.maximize_window()
-        driver.get('https://github.com/login')
+            driver.maximize_window()
+            driver.get('https://github.com/login')
 
-        username_element = driver.find_element(By.ID, 'login_field')
-        username_element.clear()
-        username_element.send_keys(username)
+            try:
+                username_element = driver.find_element(By.ID, 'login_field')
+                username_element.clear()
+                username_element.send_keys(username)
 
-        password_element = driver.find_element(By.ID, 'password')
-        password_element.clear()
-        password_element.send_keys(password)
+                password_element = driver.find_element(By.ID, 'password')
+                password_element.clear()
+                password_element.send_keys(password)
 
-        sign_in_button = driver.find_element(By.CLASS_NAME, 'js-sign-in-button')
-        sign_in_button.click()
+                sign_in_button = driver.find_element(By.CLASS_NAME, 'js-sign-in-button')
+                sign_in_button.click()
+
+                try:
+                    error_message = WebDriverWait(driver, 2).until(
+                        EC.presence_of_element_located((By.CLASS_NAME, 'js-flash-alert'))
+                    )
+                    print("Incorrect username or password")
+
+                except TimeoutException:
+                    authenticated = True
+
+                except NoSuchElementException:
+                    authenticated = True
+                    print("Login Successful")
+
+                if authenticated:
+
+                    profile_button = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.CLASS_NAME, 'AppHeader-user'))
+                    )
+                    profile_button.click()
+
+                    repo_button = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.XPATH,
+                                                    f'//span[@data-view-component="true" and contains(@class, "ActionListItem-label") and contains(text(), "Your repositories")]'))
+                    )
+                    repo_button.click()
+
+            finally:
+                driver.quit()
+            
+    def close_app(self):
+        QApplication.quit()
 
 
 if __name__ == "__main__":
